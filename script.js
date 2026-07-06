c8f398dfe05d76ffd0900374a0b2af08b6d352b7
lucastait-aurax67 <lucas.taitromancini.correa@escola.pr.gov.br>
2026-07-06 14:42:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const botao = document.getElementById("modoEscuro");
 
+botao.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+function mostrarDados(){
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+
+    document.getElementById("resultado").innerHTML =
+        "Olá, " + nome + " você tem " + idade + " anos.";
+}
